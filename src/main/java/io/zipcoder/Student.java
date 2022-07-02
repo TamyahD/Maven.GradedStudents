2c4af53f8581ca7e545af0a82773ef909183d6d9
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-02 17:02:00 -0400

    added instance variables, a Student constructor with params, getters/setters, etc...& addExamScore method

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,66 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores = new ArrayList<>();
+
+
+    public Student() {
+
+    }
+
+    public Student(String fName, String lName, Double[] testScores) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.examScores = new ArrayList(Arrays.asList(testScores));
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumberOfExamsTaken() {
+        return 0; //TODO
+    }
+
+
+    public String getExamScores() {
+        /* string to store exam score value(s) */
+        String examScore = "";
+        /* int var to count number of exams */
+        int counter = 0;
+
+        /*
+        for each to iterate through arraylist &
+        access double(type) scores
+        */
+        for (Double score : examScores) {
+            counter++;
+            examScore += "Exam " +counter + "-->" +
+                    score +"\n";
+        }
+        return examScore.toString();
+    }
+
+    public void addExamScore(double examScore) {
+        examScores.add(examScore);
+    }
+
+
 }
