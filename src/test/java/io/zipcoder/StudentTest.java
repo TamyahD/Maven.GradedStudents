c5a69e8d125d1776d32f28850e29123e0585ea18
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-02 17:15:22 -0400

    test cases pass for getExamScores & addExamScore

--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,45 @@
 package io.zipcoder;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class StudentTest {
 
+    @Test
+    public void testGetExamScores() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        String expected= (
+                "Exam 1-->100.0\n" +
+                "Exam 2-->95.0\n" +
+                "Exam 3-->123.0\n" +
+                "Exam 4-->96.0\n");
+        System.out.println("Student expected score:\n"+expected);
+        // When
+        String actual = student.getExamScores();
+
+        // Then
+        System.out.println("Actual:\n"+actual);
+        Assert.assertEquals(expected ,actual);
+    }
+
+    @Test
+    public void addExamScore() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        student.addExamScore(100.0);
+        String actual = student.getExamScores();
+
+        // Then
+        System.out.println(actual);
+    }
 }
